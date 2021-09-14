9d1b73cfafbf698538621b54dd9ca388e3d7b8d6
VortexHD <vortexhdwhite@gmail.com>
2021-09-14 18:12:13 +0100

    Revert "venus: Include firmware"
    This doesn't work :(
    This reverts commit b47836a878adfa262774569622d232539287b9e8.

--- dot_venus.mk
+++ dot_venus.mk
@@ -25,6 +25,3 @@ PRODUCT_MODEL := Mi 11
 PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
-
-# Include firmware
-$(call inherit-product, vendor/xiaomi/firmware/venus/firmware.mk)
